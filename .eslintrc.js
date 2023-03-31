61fa198fec9def5d7b95489442e0d366b885c4a4
MoshiProject <129470823+MoshiProject@users.noreply.github.com>
2023-03-31 02:35:04 -0500

    Revert "Built Core Website with product, collection, and cart pages"

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,3 @@
-/**
- * @type {import("@types/eslint").Linter.BaseConfig}
- */
 module.exports = {
-  extends: [
-    'plugin:hydrogen/recommended',
-    'plugin:hydrogen/typescript',
-    'prettier',
-  ],
-  plugins: ['prettier'],
-  rules: {'prettier/prettier': ['error', {endOfLine: 'auto'}]},
+  extends: ['plugin:hydrogen/recommended'],
 };
