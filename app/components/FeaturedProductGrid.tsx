e89a580c81f34a29d44e77e522f2e82a89586b63
Parlaxz <moshimoshiistore@gmail.com>
2023-10-15 14:19:53 -0500

    fixed back page glitch

--- app/components/FeaturedProductGrid.tsx
+++ app/components/FeaturedProductGrid.tsx
@@ -42,7 +42,7 @@ export function FeaturedProductGrid({
                 inView: {opacity: 1},
                 outOfView: {opacity: 0},
               }}
-              className="snap-start w-fit mx-0.5 h-fit"
+              className="snap-start mx-0.5 h-fit"
             >
               <ProductCard product={product} />
             </motion.div>
