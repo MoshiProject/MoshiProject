9e9797f160de31b9f0858a118749dbdd0500ab0d
Parlaxz <moshimoshiistore@gmail.com>
2023-11-25 15:47:08 -0600

    MS Clarity

--- app/root.tsx
+++ app/root.tsx
@@ -139,6 +139,7 @@ export default function App() {
         <Links />
         <FacebookPixel />
         <InspectletSnippet />
+        <MSClaritySnippet />
       </head>
       <body>
         <Layout title={name}>
@@ -227,7 +228,21 @@ const InspectletSnippet = () => {
     ></script>
   );
 };
-
+const MSClaritySnippet = () => {
+  return (
+    <script
+      dangerouslySetInnerHTML={{
+        __html: `
+        (function(c,l,a,r,i,t,y){
+          c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+          t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+          y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+      })(window, document, "clarity", "script", "jw7xr9b2sm");
+`,
+      }}
+    ></script>
+  );
+};
 const KlayivoSnippet = () => {
   return (
     <script
